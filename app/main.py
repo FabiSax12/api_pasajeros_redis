80342d91dfe134cd217d1e58bbc58a36f700c791
Marcos Rodríguez Ovares <mrodriguezovares@gmail.com>
2025-10-28 15:22:15 -0600

    Agregado caché

--- app/main.py
+++ app/main.py
@@ -1,13 +1,25 @@
+import asyncio
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
 app = FastAPI()
 
+from app.cache.redis import check_redis_connection
 from app.routes.pasajeros import pasajeros
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup code
+    await check_redis_connection()
+    yield
+    # Shutdown code
+    await app.state.redis_client.close() 
 
 app = FastAPI(
     title="API de Pasajeros",
     description="Consulta de rutas de transporte público",
-    version="1.0.0"
+    version="1.0.0",
+    lifespan=lifespan
 )
 
 app.include_router(pasajeros, prefix="/api", tags=["Pasajeros"])
